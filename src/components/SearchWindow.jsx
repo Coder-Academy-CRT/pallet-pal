29f30760162ad987d228b692b45b2b6b96a9e5e1
mupersega <cambodiam@hotmail.com>
2022-02-01 08:17:23 +1000

    set up appropriate css variables, added basic on off state to search window(experimental)

--- src/components/SearchWindow.jsx
+++ src/components/SearchWindow.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
 function SearchWindow() {
-    return <div id='searchWindow'>SEARCH</div>
+    // added this state to test how turning a sidebar component on and off might work.
+    // this works from inside the component, however I am unsure how to link the setOn() to the icon in the sidebarNav which is a separate component.
+    const [active, setActive] = useState(true)
+
+    function setOff() {
+        setActive(false)
+    }
+
+    function setOn() {
+        setActive(true)
+    }
+
+    // only render while active
+    if (active) {
+        return (
+            <div id='searchWindow'>
+                <button onClick={setOff}>close search</button>
+                SEARCH
+            </div>
+        )
+    } else {
+        // have put a button here as a placeholder to test opening and closing.
+        return <button onClick={setOn}>open search</button>
+    }
 }
 
 export default SearchWindow
