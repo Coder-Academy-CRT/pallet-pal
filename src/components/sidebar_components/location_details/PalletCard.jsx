97bc81c5b90c9386f0484940c7af793a752ea8e9
Rhona Lam <rainie.gi.lam@gmail.com>
2022-02-01 17:24:15 +1000

    Added pallet id as title for readability

--- src/components/sidebar_components/location_details/PalletCard.jsx
+++ src/components/sidebar_components/location_details/PalletCard.jsx
@@ -1,12 +1,18 @@
 import ProductCard from "./ProductCard"
 
-function PalletCard() {
+function PalletCard({ palletId }) {
     const num = Math.floor(Math.random() * 5) + 1
     const productCards = []
     for (let i = 0; i < num; i++) {
         productCards.push(<ProductCard />)
     }
-    return <div className='palletCard'>{productCards}</div>
+    return (
+        <div className='palletCard' 
+            palletId={palletId}><span style={{color: "white", fontWeight: "bold"}}>Pallet #{palletId}</span>
+            {productCards}
+        </div>
+    )
+
 }
 
 export default PalletCard
