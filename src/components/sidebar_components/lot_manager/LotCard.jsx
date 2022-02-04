dbb7aa59f334c59c32aa6be2604fc78e57c961c9
tfxl <CAB022105@coderacademy.edu.au>
2022-02-04 16:12:01 +1000

    Additional styling and logic for LotCard

--- src/components/sidebar_components/lot_manager/LotCard.jsx
+++ src/components/sidebar_components/lot_manager/LotCard.jsx
@@ -11,37 +11,25 @@ function LotCard( {lot} ) {
         setEditMode(false)
     }
 
-    const button_style = { 
-        backgroundColor: "lightblue",
-        width: "45px",
-        height: "30px",
-        borderRadius: "60px"
-    }
-
-    console.log("BYE")
-    console.log(lot)
 
     let bag_sizes = []
     let total_amount = 0
 
     for (let property in lot) {
-        
         if (property != "lot_code" && property != "seed_variety" && property != "seed_type") {
             bag_sizes.push(
                 <li>
-                    {(`- - - - - - - - ${property}kg bags :`) + `${lot[property]}kg`}    
+                    {(`${property}kg bags:    `) + `${lot[property]}kg`}    
                 </li>
             )
             total_amount += lot[property]
-        }
-        
+        }   
     }
 
 
     if (editMode) {
         return (
             <div className='lotCard'>
-                
                 <h3>insert lot</h3>
                 <h3>dropdown</h3>
                 <button onClick={setEditOff}>save</button>
@@ -49,18 +37,22 @@ function LotCard( {lot} ) {
         )
     } else {
         return (
-            <div className='lotCard'>
-
-                <h2>{lot.lot_code}</h2>
-                <ul>
-                    { bag_sizes }
-                </ul>
-        
-                <h3>{ total_amount = "0" ? "no stock" : `Total : ${total_amount} kg` }</h3>
-
-                <button onClick={setEditOn} style={button_style}>edit</button>
-
-            </div>
+            <>
+                <div className='lotCard'>
+                    <div id="lotCardLhs">
+                        <button onClick={setEditOn}>edit</button>
+                        <h2>{lot.lot_code}</h2>
+                    </div>
+                    <div id="lotCardRhs">
+                        <ul>
+                            { bag_sizes }
+                        </ul>
+                        <h3>{
+                            total_amount == "0" ? "no stock" : `Total: ${total_amount} kg` }
+                        </h3>
+                    </div>
+                </div>
+            </>
         )
     }
 }
