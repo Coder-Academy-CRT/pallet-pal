2c72e4e682b64675410f85cc0fb698c248d6082a
tfxl <CAB022105@coderacademy.edu.au>
2022-02-04 15:14:25 +1000

    Lot cards showing lots and amounts with very basic style. Edit function not coded yet

--- src/components/sidebar_components/lot_manager/LotCard.jsx
+++ src/components/sidebar_components/lot_manager/LotCard.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function LotCard() {
+function LotCard( {lot} ) {
     const [editMode, setEditMode] = useState(false)
 
     function setEditOn() {
@@ -11,9 +11,37 @@ function LotCard() {
         setEditMode(false)
     }
 
+    const button_style = { 
+        backgroundColor: "lightblue",
+        width: "45px",
+        height: "30px",
+        borderRadius: "60px"
+    }
+
+    console.log("BYE")
+    console.log(lot)
+
+    let bag_sizes = []
+    let total_amount = 0
+
+    for (let property in lot) {
+        
+        if (property != "lot_code" && property != "seed_variety" && property != "seed_type") {
+            bag_sizes.push(
+                <li>
+                    {(`- - - - - - - - ${property}kg bags :`) + `${lot[property]}kg`}    
+                </li>
+            )
+            total_amount += lot[property]
+        }
+        
+    }
+
+
     if (editMode) {
         return (
             <div className='lotCard'>
+                
                 <h3>insert lot</h3>
                 <h3>dropdown</h3>
                 <button onClick={setEditOff}>save</button>
@@ -22,9 +50,16 @@ function LotCard() {
     } else {
         return (
             <div className='lotCard'>
-                <p>lot number</p>
-                <p>amt.</p>
-                <button onClick={setEditOn}>edit</button>
+
+                <h2>{lot.lot_code}</h2>
+                <ul>
+                    { bag_sizes }
+                </ul>
+        
+                <h3>{ total_amount = "0" ? "no stock" : `Total : ${total_amount} kg` }</h3>
+
+                <button onClick={setEditOn} style={button_style}>edit</button>
+
             </div>
         )
     }
--- src/components/sidebar_components/lot_manager/LotList.jsx
+++ src/components/sidebar_components/lot_manager/LotList.jsx
@@ -1,12 +1,40 @@
-import React from "react"
+import React, { useContext} from "react"
+import palletpalContext from "../../../palletpalContext"
 import LotCard from "./LotCard"
 
+
 function LotList() {
+
+    const { state: { lots, products } } = useContext(palletpalContext)
+
+    let all_lots = []
+    
+    {lots.forEach( (lot, index) => {
+
+        products.forEach( (product) => {
+            if (product.lot_code == lot.lot_code) {
+                
+                let product_amount = Number(product.bag_size) * Number(product.number_of_bags)
+                
+                switch (lot[product.bag_size]) {
+                    case undefined:
+                        lot[product.bag_size] = 0
+                        lot[product.bag_size] += product_amount
+                        break
+                    case true:
+                        lot[product.bag_size] += product_amount
+                        break
+                }        
+            }
+        })
+
+        all_lots.push( <LotCard key={index} lot={lot}/> )
+    })}   
+
+
     return (
         <div id='lotList'>
-            <LotCard />
-            <LotCard />
-            <LotCard />
+            {all_lots}
         </div>
     )
 }
