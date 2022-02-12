99b5871f90f45760ed1798c3b5a0603a86f9458a
Rhona Lam <rainie.gi.lam@gmail.com>
2022-02-12 16:22:37 +1000

    netlify tested, removed the baseURL from api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
-    //baseURL: "https://obscure-bayou-76349.herokuapp.com/"
 })
 
 export default api
